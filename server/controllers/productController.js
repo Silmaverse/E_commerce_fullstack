ded2f974792d53502faa8940a0548fb9b64daa3a
silma subah <silmasubah105@gmail.com>
2026-06-08 17:28:48 +0600

    msg

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,9 +1,14 @@
+const { uploadToCloudinary } = require("../helpers/util");
 const categorySchema = require("../models/categorySchema");
-const { product } = require("../models/prodcutSchema");
+const { product, product } = require("../models/prodcutSchema");
 
 const createProduct=async(req,res)=>{
     try{
      const{title,slug,description,category,price,discountpercentage,variants,tags,isActive}=req.body;
+
+     const thumbnail=req.files?.thumbnail;
+     const images=req.files?.images;
+
      if(!title) return res.status(400).send({messgae:"Prodcut Tilte is required"});
      if(!slug) return res.status(400).send({messgae:"slug is required"});
      const isSlugExist=await product.findOne({
@@ -16,7 +21,8 @@ const createProduct=async(req,res)=>{
      if(!isCategoryExist) return res.status(400).send({messsage:"Invalid Category"});
      if(!price) return res.status(400).send({messsage:"Prodcut Price is required"});
 
-     const variantsdata=variants;
+     const variantsdata=JSON.parse(variants);
+
      if(!Array.isArray(variantsdata)|| variantsdata.length==0) 
         return res.status(400).send({message:"Minumum 1 variant is required"})
     for(const variant of variants){
@@ -29,6 +35,43 @@ const createProduct=async(req,res)=>{
     const skus=variantsdata.map((v)=>v.sku);
     if(new Set(skus).size !== skus.length )
         return  res.status(400).send({messgae:"SKU must be unique"});
+
+    //image validation and upload
+
+    if(!thumbnail || thumbnail.length==0)
+        return res.status(400).send({message:"Product thumbnail is required"});
+
+    if(!images || images.length==0)
+        return res.status(400).send({message:"Product Image is required"});
+
+    const thumbnailurl= await uploadToCloudinary({
+       mimetype:thumbnail[0].mimetype, 
+       imgbuffer:thumbnail[0].imgbuffer
+    });
+
+    const imgsres = images.map(async (item)=>{
+      return uploadToCloudinary({
+        mimetype:item.mimetype,
+        imgBuffer:item.buffer,
+      })  
+    });
+    
+    const imagesUrls=await Promise.all(imgsres);
+
+    const productdata=await product.create({
+        title,
+        slug,
+        description,
+        category,
+        price,
+        discountpercentage,
+        variants:variantsdata,
+        tags,
+        isActive,
+        thumbnail:thumbnailurl,
+        images:imagesUrls,
+    });
+    res.status(200).send({messgae:"Product created successfully",productdata});
     }catch(error){
         console.log(error)
     }
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,8 +1,16 @@
 const { createProduct } = require("../controllers/ProductController");
 const { authMiddleware, roleCheck } = require("../middleware/authMiddleware");
+const multer = require("multer");
+const upload = multer();
 
-const route=require("express").Router();
+const route = require("express").Router();
 
-route.post("/create",authMiddleware,roleCheck(["admin","moderator"]),createProduct);
+route.post(
+  "/create",
+  authMiddleware,
+  roleCheck(["admin", "moderator"]),
+  upload.fields([{ name: "thumbnail", maxcount: 1 },{name:"images",maxcount:4}]),
+  createProduct,
+);
 
-module.exports=route;
+module.exports = route;
